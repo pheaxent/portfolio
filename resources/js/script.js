5fd6c0875ecfd431373bd5ed8d929a37a4463680
Ubaid <pheaxent@gmail.com>
2021-01-03 08:15:35 +0500

    Add accordion for FAQ section

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,7 +4,7 @@ let form = document.getElementById('form');
 let nameInputElement = document.getElementById('name');
 
 const nameValidation = () => {
-    if (typeof Number(nameInputElement.value) == 'number') {
+    if (!isNaN(nameInputElement.value)) {
         alert('Please enter a valid name. e.g Ubaid Aslam');
     }
 };
@@ -12,3 +12,23 @@ const nameValidation = () => {
 form.addEventListener('submit', nameValidation);
 
 // FAQ Section Accordion Creation
+
+// get all accordions
+let accordions = document.querySelectorAll(".accordion");
+console.log(accordions);
+function createAccordion() {
+    // get attribute value
+    let ariaAttribute = this.getAttribute('aria-expanded');
+
+    // change all accordions aria-expanded attribute value into false.
+    for (let i = 0; i < accordions.length; i++) {
+        accordions[i].setAttribute('aria-expanded', 'false');
+    }
+
+    // check if aria-expanded value is false.
+    if (ariaAttribute === 'false') {
+        this.setAttribute('aria-expanded', 'true');
+    }
+}
+
+accordions.forEach(accordion => accordion.addEventListener('click', createAccordion));
